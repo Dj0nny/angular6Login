99c456323b4500afe324215d5157449391f22b65
Dj0nny <frank96990@gmail.com>
2018-08-30 18:56:48 +0200

    add auth service and basic app routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AdminComponent } from './admin/admin.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'admin',
+        component: AdminComponent
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
